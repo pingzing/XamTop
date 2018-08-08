5500a90e764becbf85fe453fcfc11d287b0f2e63
pingzing <mcalistern@gmail.com>
2018-08-09 00:57:12 +0300

    exploration into breaking context menu out into its own library

---
+++ src/ContextMenu/Abstractions/IContextMenu.Shared.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContextMenu.Abstractions
+{
+    public interface IContextMenu
+    {
+        IEnumerable<IContextMenuItem> ItemsSource { get; set; }
+    }
+}
---
+++ src/ContextMenu/Abstractions/IContextMenuButton.Shared.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContextMenu.Abstractions
+{
+    public interface IContextMenuButton : IContextMenuItem
+    {
+        /// <summary>
+        /// The text to display for the context menu item.
+        /// </summary>
+        string Label { get; set; }
+
+        /// <summary>
+        /// Occurs when the user clicks this context menu item.
+        /// </summary>
+        EventHandler Clicked { get; set; }
+    }
+}
